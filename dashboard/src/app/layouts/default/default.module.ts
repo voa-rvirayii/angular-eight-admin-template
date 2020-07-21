d0012ffb1d6b4cc35d0e73533ede6ad25605c9e5
unknown <robertovirayii@gmail.com>
2020-07-21 22:21:13 +0800

    rviray003 update routing

--- dashboard/src/app/layouts/default/default.module.ts
+++ dashboard/src/app/layouts/default/default.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from "@angular/common";
 import { DefaultComponent } from "./default.component";
 import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
 
+import { RouterModule } from "@angular/router";
+
 @NgModule({
   declarations: [DefaultComponent, DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class DefaultModule {}
