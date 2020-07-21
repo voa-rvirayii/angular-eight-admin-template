a7b3e9e98990be55910b1517c936f44e82fed24b
unknown <robertovirayii@gmail.com>
2020-07-21 22:05:17 +0800

    rviray002 routing update

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DefaultComponent } from "./layouts/default/default.component";
+import { DashboardComponent } from "./modules/dashboard/dashboard.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "",
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
