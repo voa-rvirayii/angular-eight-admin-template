afaf60bf797bb23e30a2f35e753382aed4035a36
unknown <robertovirayii@gmail.com>
2020-07-22 00:28:28 +0800

    rviray004 shared module 14-18

--- dashboard/src/app/layouts/default/default.module.ts
+++ dashboard/src/app/layouts/default/default.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DefaultComponent } from "./default.component";
 import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
-
+import { PostsComponent } from "src/app/modules/posts/posts.component";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [DefaultComponent, DashboardComponent],
+  declarations: [DefaultComponent, DashboardComponent, PostsComponent],
   imports: [CommonModule, RouterModule],
 })
 export class DefaultModule {}
